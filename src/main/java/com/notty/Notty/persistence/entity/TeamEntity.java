63843a21e6258a34a3dac965d3a06f4d62208e9f
Sebastian609 <62573264+Sebastian609@users.noreply.github.com>
2024-04-13 19:46:42 -0500

    Svillar: TeamEntity table created

---
+++ src/main/java/com/notty/Notty/persistence/entity/TeamEntity.java
@@ -0,0 +1,26 @@
+package com.notty.Notty.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "team")
+@Getter
+@Setter
+@NoArgsConstructor
+public class TeamEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_team",nullable = false)
+    private Integer idTask;
+    @Column(length = 30,nullable = false)
+    private String name;
+    @Column(name = "created_at",nullable = false, columnDefinition = "DATETIME")
+    private Timestamp createrAt;
+    @Column(name = "active_team",nullable = false, columnDefinition = "TINYINT")
+    private Boolean activeTeam;
+}
